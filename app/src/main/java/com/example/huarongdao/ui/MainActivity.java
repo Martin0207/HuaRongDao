6ac0b648371de297a5740967ef91d19e253b4eac
martin <martin0207mfh@163.com>
2020-03-17 11:09:53 +0800

    首页返回按钮功能

--- app/src/main/java/com/example/huarongdao/ui/MainActivity.java
+++ app/src/main/java/com/example/huarongdao/ui/MainActivity.java
@@ -57,6 +57,7 @@ public class MainActivity extends BaseActivity {
         ivBack.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                finish();
             }
         });
     }
